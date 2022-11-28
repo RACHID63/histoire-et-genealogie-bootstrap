161c61f6908ea9ea04820368c41ae11f9317afec
LAIEB Rachid <rachid.laieb@gmail.com>
2022-11-28 16:15:53 +0100

    link btn page genealogilaieb

--- geneLaiebBouchareb.js
+++ geneLaiebBouchareb.js
@@ -1,36 +1,3 @@
-const ligneeMaternel = document.getElementById("lignéematernel");
-const ligneePaternel = document.getElementById("lignéepaternel");
-const introduction = document.getElementById("introduction");
-const pargraph2 = document.getElementById("text2")
 
-// action sur bouton maternel
-function visibleMaternel() {
-  ligneeMaternel.style.visibility = "visible";
-  ligneePaternel.style.visibility = "hidden";
-  introduction.style.visibility = "hidden";
-}
 
-btnMaternel = document.getElementById("btnMaternel");
-btnMaternel.addEventListener("click", visibleMaternel);
 
-// action sur bouton paternel
-
-function visiblePaternel() {
-  ligneePaternel.style.visibility = "visible";
-  ligneeMaternel.style.visibility = "hidden";
-  introduction.style.visibility = "hidden";
-}
-
-btnPaternel = document.getElementById("btnPaternel");
-btnPaternel.addEventListener("click", visiblePaternel);
-
-// action sur bouton acceuil
-
-function visibleAceuil() {
-  ligneePaternel.style.visibility = "hidden";
-  ligneeMaternel.style.visibility = "hidden";
-  introduction.style.visibility = "visible";
-}
-
-btnAcceuil = document.getElementById("btnAcceuil");
-btnAcceuil.addEventListener("click", visibleAceuil);
